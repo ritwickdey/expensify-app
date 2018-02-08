503cc0f022ea4cf3c0ecbba82b5941139c860d3b
Ritwick Dey <ritwickdey@outlook.com>
2018-02-09 01:24:27 +0530

    route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
 import './styles/style.scss';
 
-ReactDOM.render(<p>Hello World!</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => <p>This is from dashboard Component</p>;
+
+const AddExpensePage = () => <p>This is from Create Component</p>;
+
+const EditExpensePage = () => <p>This is from Edit Component</p>;
+
+const HelpExpensePage = () => <p>This is from help Component</p>;
+
+const NotFoundPage = () => (
+  <p>
+    404! Not Found <Link to="/">Go To Homepage</Link>
+  </p>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <Link to="/">Dashboard</Link>
+    <Link to="/create">Create Expense</Link>
+    <Link to="/edit">Edit Expense</Link>
+    <Link to="/help">Help</Link>
+  </header>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" exact={true} component={ExpenseDashboardPage} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpExpensePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
