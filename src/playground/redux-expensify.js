7bd394727c5c83bd2d86a2fe2ae6572c7a70ff1c
Ritwick Dey <ritwickdey@outlook.com>
2018-02-10 15:57:20 +0530

    planning

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,17 @@
 import { createStore, combineReducers } from 'redux';
 
+/**
+ * Operations :-
+ *    ADD_EXPENSE
+ *    REMOVE_EXPENSE
+ *    EDIT_EXPENSE
+ *    SET_TEXT_FILTER
+ *    SORT_BY_DATE
+ *    SORT_BY_AMOUNT
+ *    SET_START_DATE
+ *    SET_END_DATE
+ */
+
 const demoState = {
   expenses: [
     {
