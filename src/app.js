8ba27e11964b936ab99464c1e843d1c5ca92b652
Ritwick Dey <ritwickdey@outlook.com>
2018-02-11 14:57:25 +0530

    app.js setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRouter } from './routes/AppRouter';
+import { store } from './store/configureStore';
+import { addExpense, removeExpense } from './actions/expenses';
+import { getFilteredExpense } from './selectors/expenses';
+import { setTextFilter, sortByAmount } from './actions/filters';
 
 import './styles/style.scss';
+
+const appStore = store();
+
+appStore.subscribe(() => {
+  const state = appStore.getState();
+  console.log(getFilteredExpense(state.expenses, state.filter));
+});
+
+const expense1 = appStore.dispatch(
+  addExpense({ amount: 7000, description: 'Rent Bill', createdAt: 100 })
+);
+
+const expense2 = appStore.dispatch(
+  addExpense({ amount: 2500, description: 'Gas Bill', createdAt: 75 })
+);
+
+const expense3 = appStore.dispatch(
+  addExpense({ amount: 459, description: 'Coffee Bill', createdAt: -50 })
+);
+
+// appStore.dispatch(removeExpense({ id: expense1.expense.id }));
+appStore.dispatch(setTextFilter('Bill'));
+appStore.dispatch(sortByAmount());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
