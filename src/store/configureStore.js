a44e4d1a6c80643cd0ee9351a09c1be92a67d387
Ritwick Dey <ritwickdey@outlook.com>
2018-02-11 23:54:05 +0530

    configure for redux extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,5 +7,6 @@ export const store = () =>
     combineReducers({
       expenses: expensesReducer,
       filters: filterReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
