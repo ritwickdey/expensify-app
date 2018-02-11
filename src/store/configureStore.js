e6534c9557febc27e3acd55a4c9ff176217296eb
Ritwick Dey <ritwickdey@outlook.com>
2018-02-11 13:25:57 +0530

    separated store

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+import { expensesReducer } from '../reducers/expenses';
+import { filterReducer } from '../reducers/filters';
+
+export const store = () =>
+  createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filter: filterReducer
+    })
+  );
