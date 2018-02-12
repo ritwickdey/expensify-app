f25aa7a7d93ea607ac0997dc1ce0e3f84c75b8b7
Ritwick Dey <ritwickdey@outlook.com>
2018-02-13 00:19:54 +0530

    test for SortByDate, sortByAmount, setTextFilter

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -22,3 +22,34 @@ test('should generate set end date action', () => {
     date: moment(0)
   });
 });
+
+test('should generate correct type for sort By Date', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  });
+});
+
+test('should generate correct type for sort by amount', () => {
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type: 'SORT_BY_AMOUNT'
+  });
+});
+
+test('should set Text Filter with correct text value', () => {
+  const text = 'foo...';
+  const action = setTextFilter(text);
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text
+  });
+});
+
+test('should set Text Filter with default value', () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  });
+});
