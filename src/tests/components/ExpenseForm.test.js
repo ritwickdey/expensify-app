4495970ca37ab0ebf5446569aa18e07fef2cd6f8
Ritwick Dey <ritwickdey@outlook.com>
2018-02-13 22:40:11 +0530

    Test for ExpenseForm on Submit

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -27,38 +27,61 @@ test('should render error for invalid form submission', () => {
 
 test('should set description on input change', () => {
   const wrapper = shallow(<ExpenseForm />);
-  const value = 'foo-value'
-  wrapper.find('input').at(0).simulate('change', {
-    target: {value}
-  });
+  const value = 'foo-value';
+  wrapper
+    .find('input')
+    .at(0)
+    .simulate('change', {
+      target: { value }
+    });
   expect(wrapper.state('description')).toBe(value);
 });
 
 test('should set note on textarea change', () => {
   const wrapper = shallow(<ExpenseForm />);
-  const value = 'foo-note'
+  const value = 'foo-note';
   wrapper.find('textarea').simulate('change', {
-    target: {value}
+    target: { value }
   });
   expect(wrapper.state('note')).toBe(value);
 });
 
-
 test('should set amount if valid input', () => {
   const wrapper = shallow(<ExpenseForm />);
-  const value = '125.25'
-  wrapper.find('input').at(1).simulate('change', {
-    target: {value}
-  });
+  const value = '125.25';
+  wrapper
+    .find('input')
+    .at(1)
+    .simulate('change', {
+      target: { value }
+    });
   expect(wrapper.state('amount')).toBe(value);
 });
 
 test('should NOT set amount if invalid input', () => {
   const wrapper = shallow(<ExpenseForm />);
-  const value = 'foo-abcsjd'
-  wrapper.find('input').at(1).simulate('change', {
-    target: {value}
-  });
+  const value = 'foo-abcsjd';
+  wrapper
+    .find('input')
+    .at(1)
+    .simulate('change', {
+      target: { value }
+    });
   expect(wrapper.state('amount')).toBeFalsy();
 });
 
+test('should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn();
+  const expense = { ...expenses[0] };
+  delete expense.id;
+  const wrapper = shallow(
+    <ExpenseForm expense={expense} onSubmit={onSubmitSpy} />
+  );
+
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
+
+  expect(wrapper.state('error')).toBeFalsy();
+  expect(onSubmitSpy).toHaveBeenCalledWith(expense);
+});
