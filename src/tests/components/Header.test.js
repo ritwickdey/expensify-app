e24230e0bf2cf4fc90ec0662de32b9fb3c4962f4
Ritwick Dey <ritwickdey@outlook.com>
2018-02-13 17:55:02 +0530

    header test changed

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import reactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+
 import { Header } from '../../components/Header';
 
 test('should render header correctly', () => {
-  const renderer = new reactShallowRenderer();
-  renderer.render(<Header />);
-
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
-}); 
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find('h1').length).toBe(1);
+});
