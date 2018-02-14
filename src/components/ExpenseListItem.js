c4eaf08aaaf8a10c96d3b4e1da491709829aa9e3
Ritwick Dey <ritwickdey@outlook.com>
2018-02-14 15:37:33 +0530

    removed unnessary code

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { removeExpense } from '../actions/expenses';
@@ -17,4 +16,4 @@ export const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
