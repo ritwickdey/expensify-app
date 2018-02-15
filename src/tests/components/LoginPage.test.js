7c0e1c630b7edbdd017336c9a72da53f0b3b8ff4
Ritwick Dey <ritwickdey@outlook.com>
2018-02-16 01:05:09 +0530

    snapshot test for login page

---
+++ src/tests/components/LoginPage.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { LoginPage } from '../../components/LoginPage';
+
+test('should render Login Page correctly', () => {
+  const wrapper = shallow(<LoginPage />);
+  expect(wrapper).toMatchSnapshot();
+});
