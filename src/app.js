d1ac14ce4d68dab9245840a26748a21f759e3b42
Ritwick Dey <ritwickdey@outlook.com>
2018-02-16 11:53:31 +0530

    cleanup

--- src/app.js
+++ src/app.js
@@ -4,42 +4,21 @@ import { Provider } from 'react-redux';
 import 'react-dates/initialize';
 
 import { store } from './store/configureStore';
-import {
-  addExpense,
-  removeExpense,
-  startSetExpenses
-} from './actions/expenses';
 import { getFilteredExpense } from './selectors/expenses';
 import { setTextFilter, sortByAmount } from './actions/filters';
 import { AppRouter, history } from './routes/AppRouter';
 import { firebase } from './firebase/firebase';
 
+import {
+  addExpense,
+  removeExpense,
+  startSetExpenses
+} from './actions/expenses';
+
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/style.scss';
+
 const appStore = store();
-
-appStore.subscribe(() => {
-  const state = appStore.getState();
-  console.log(state.filters);
-  console.log(getFilteredExpense(state.expenses, state.filters));
-  console.log('\n');
-});
-
-const expense1 = appStore.dispatch(
-  addExpense({ amount: 7000, description: 'Rent Bill', createdAt: 100 })
-);
-
-const expense2 = appStore.dispatch(
-  addExpense({ amount: 2500, description: 'Gas Bill', createdAt: 75 })
-);
-
-const expense3 = appStore.dispatch(
-  addExpense({ amount: 45900, description: 'Coffee Bill', createdAt: -50 })
-);
-
-// appStore.dispatch(removeExpense({ id: expense1.expense.id }));
-// appStore.dispatch(setTextFilter('Bill'));
-// appStore.dispatch(sortByAmount());
 
 const jsx = (
   <Provider store={appStore}>
@@ -61,7 +40,7 @@ firebase.auth().onAuthStateChanged(user => {
   if (user) {
     appStore.dispatch(startSetExpenses()).then(() => renderApp());
     if (history.location.pathname === '/') history.push('/dashboard');
-    console.log('logged in');
+    console.log('logged in', user);
   } else {
     renderApp();
     history.push('/');
