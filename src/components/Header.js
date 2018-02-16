e1cf36991860e2f56afceb603953deaadb6758dd
Ritwick Dey <ritwickdey@outlook.com>
2018-02-16 20:54:39 +0530

    header designed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { startLogout } from '../actions/auth';
 import { connect } from 'react-redux';
 
 export const Header = props => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/dashboard" exact={true} activeClassName="is-active">
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create Expense
-    </NavLink>
-    <button onClick={props.startLogout}>Logout</button>
+  <header className="header">
+    <div className="container">
+      <div className="header__container">
+        <Link className="header__title" to="/dashboard" exact={true}>
+          <h1>Expensify</h1>
+        </Link>
+        <button className="btn btn__link" onClick={props.startLogout}>Logout</button>
+      </div>
+    </div>
   </header>
 );
 
